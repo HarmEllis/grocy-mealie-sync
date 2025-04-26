7c1059fcabf65d6bf70a86675ac7260bfaf959c8
Harm Ellis <harmellis@outlook.com>
2025-04-26 08:22:49 +0000

    Updated the .env example and implemented the dotenv package for development.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import logger from './helpers/logger';
 
-logger.error('Hello, world!');
+if (process.env.NODE_ENV !== 'production') {
+  // The .env is only used in development
+  // Container environment variables should be used in production
+  require('dotenv').config();
+}
+
+logger.info('Hello, world!');
