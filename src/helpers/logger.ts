7268cf2c67cd97f0b693bfe3b4d4f5921b610d90
Harm Ellis <harmellis@outlook.com>
2025-04-27 19:33:13 +0000

    Add env var to set the log level.

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,12 +1,16 @@
 import winston from 'winston';
 
+const loglevel = process.env.LOG_LEVEL || 'info'; // Default to 'info' if not set
+
 const logger = winston.createLogger({
-  level: 'info',
+  level: loglevel, // Set the default log level to 'info'
   format: winston.format.combine(
     winston.format.timestamp(), // Add a timestamp to each log
     winston.format.json(), // Format the logs as JSON
   ),
   transports: [new winston.transports.Console()],
 });
+
+logger.info('Winston logger initialized with level ' + loglevel);
 
 export default logger;
