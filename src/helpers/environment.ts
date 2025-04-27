1be1e7af3f08cbce6355c08cb8fe399dd9b152bd
Harm Ellis <harmellis@outlook.com>
2025-04-27 13:50:01 +0000

    Add a helper to validate and retrieve environment variables.

---
+++ src/helpers/environment.ts
@@ -0,0 +1,7 @@
+export function getEnvironmentVariable(key: string): string {
+  const envVar = process.env[key];
+  if (!envVar) {
+    throw new Error(`Environment variable $'{key}' is not set`);
+  }
+  return envVar;
+}
