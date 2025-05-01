2a73278fec1db8499199564bacaa745e4002e87e
Harm Ellis <harmellis@outlook.com>
2025-05-01 18:47:37 +0000

    Replaced typed-openapi  with @hey-api/openapi-ts to generate usable clients and types.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import logger from './helpers/logger';
 import { getAllUnits } from './mealie/mealie';
 
+import { createClient } from '@hey-api/client-fetch';
+import { client, client as mealieClient } from './clients/mealie/client.gen';
+import { getAllApiUnitsGet } from './clients/mealie/sdk.gen';
+
+import { getEnvironmentVariable } from './helpers/environment';
+
 if (process.env.NODE_ENV !== 'production') {
   // The .env is only used in development
   // Container environment variables should be used in production
@@ -11,14 +17,21 @@ logger.info('Retrieving units from Mealie');
 
 async function main(): Promise<void> {
   try {
-    const units = await getAllUnits();
-    if (units.length === 0) {
+    client.setConfig({
+      baseUrl: getEnvironmentVariable('MEALIE_URL'),
+      headers: {
+        Authorization: `Bearer ${getEnvironmentVariable('MEALIE_API_KEY')}`,
+      },
+    });
+    const unitsList = await getAllApiUnitsGet();
+    const units = unitsList.data?.items;
+    if (!units || units.length === 0) {
       logger.warn('No units found in Mealie');
     } else {
       logger.info(`Retrieved ${units.length} units from Mealie`);
+      logger.info('Units retrieved successfully');
+      logger.debug('Example unit:', units.pop());
     }
-    logger.info('Units retrieved successfully');
-    logger.debug('Example unit:', units.pop());
   } catch (error) {
     logger.error('Error retrieving units from Mealie:', error);
   }
