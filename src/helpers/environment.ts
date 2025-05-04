bf26faffdd6bbcd5b07349935747afaa4c1b29c8
Harm Ellis <harmellis@outlook.com>
2025-05-04 14:45:47 +0000

    Fixed issue with not all env vars being retrieved from the .env file.

--- src/helpers/environment.ts
+++ src/helpers/environment.ts
@@ -1,3 +1,9 @@
+if (process.env.NODE_ENV !== 'production') {
+  // The .env is only used in development
+  // Container environment variables should be used in production
+  require('dotenv').config();
+}
+
 export function getEnvironmentVariable(key: string): string {
   const envVar = process.env[key];
   if (!envVar) {
