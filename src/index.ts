b4aeb8b829fc5a4cf6349755fd7cbb91361a2d32
Harm Ellis <harmellis@outlook.com>
2025-05-04 14:46:47 +0000

    Update the test function.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,10 @@
 import logger from './helpers/logger';
-import { getAllUnits } from './mealie/mealie';
-
-import { createClient } from '@hey-api/client-fetch';
-import { client, client as mealieClient } from './clients/mealie/client.gen';
-import { getAllApiUnitsGet } from './clients/mealie/sdk.gen';
-
 import { getEnvironmentVariable } from './helpers/environment';
 
-if (process.env.NODE_ENV !== 'production') {
-  // The .env is only used in development
-  // Container environment variables should be used in production
-  require('dotenv').config();
-}
-
-logger.info('Retrieving units from Mealie');
+import { client, client as mealieClient } from './clients/mealie/client.gen';
+import { getAllApiUnitsGet } from './clients/mealie/sdk.gen';
+import { fetchAllPaginatedItems } from './helpers/mealie';
+import { GetAllApiUnitsGetData, IngredientUnitOutput } from './clients/mealie/types.gen';
 
 async function main(): Promise<void> {
   try {
@@ -23,8 +14,14 @@ async function main(): Promise<void> {
         Authorization: `Bearer ${getEnvironmentVariable('MEALIE_API_KEY')}`,
       },
     });
-    const unitsList = await getAllApiUnitsGet();
-    const units = unitsList.data?.items;
+
+    logger.info('Retrieving units from Mealie');
+    const unitOptions = { query: { orderBy: 'name', perPage: 30 } };
+    const units: IngredientUnitOutput[] = await fetchAllPaginatedItems<IngredientUnitOutput>(
+      getAllApiUnitsGet,
+      unitOptions,
+    );
+
     if (!units || units.length === 0) {
       logger.warn('No units found in Mealie');
     } else {
