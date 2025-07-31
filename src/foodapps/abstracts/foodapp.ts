3092e547a4f55bfe0cfab7bc07403ec612731547
Harm Ellis <harmellis@outlook.com>
2025-07-31 19:30:07 +0000

    Add more functions to the abstracs FoodApp class.

--- src/foodapps/abstracts/foodapp.ts
+++ src/foodapps/abstracts/foodapp.ts
@@ -1,7 +1,11 @@
 import { Unit } from '../../types/foodapptypes';
 
 export abstract class FoodApp {
+  abstract toString(): string; // Returns the name of the food app
   abstract getAllUnits(): Promise<Unit[]>;
   abstract getUnitById(id: string): Promise<Unit | null>;
   abstract getUnitByName(name: string): Promise<Unit | null>;
+  abstract focUnit(name: string, pluralName: string): Promise<Unit>;
+  abstract updateUnit(unit: Unit): Promise<void>;
+  abstract syncTo(foodApp: FoodApp): Promise<void>;
 }
