578df1d34a1e5a5538b2baa6bd35b3e9cf5ccf0c
Harm Ellis <harmellis@outlook.com>
2025-08-04 10:55:56 +0000

    Moved all Unit related functions to a seperate  class and file for grocy.

--- src/food-apps/implementations/grocy/grocy-app.ts
+++ src/food-apps/implementations/grocy/grocy-app.ts
@@ -1,21 +1,10 @@
-import {
-  getObjectsByEntity,
-  GetObjectsByEntityData,
-  Options,
-  postObjectsByEntity,
-  PostObjectsByEntityData,
-  putObjectsByEntityByObjectId,
-  PutObjectsByEntityByObjectIdData,
-  QuantityUnit,
-} from '../../../api-clients/grocy';
+import { FoodAppBase } from '../../base/food-app';
 import { client } from '../../../api-clients/grocy/client.gen';
 import { getEnvironmentVariable } from '../../../utils/env';
-import logger from '../../../utils/logger';
-import { Unit } from '../../base/food-app-types';
-import { FoodAppBase } from '../../base/food-app';
-import { GrocyUnitsToUnits, unitToGrocyUnit } from './grocy-utils';
+import { GrocyUnitManager } from './grocy-unit-manager';
 
 class GrocyApp implements FoodAppBase {
+  private unitManager: GrocyUnitManager;
   constructor() {
     // Note that the Grocy API client does not return an error if the API key is invalid,
     // it will just return an empty response
@@ -25,92 +14,13 @@ class GrocyApp implements FoodAppBase {
         'GROCY-API-KEY': `${getEnvironmentVariable('GROCY_API_KEY')}`,
       },
     });
+    this.unitManager = new GrocyUnitManager();
+  }
+  getUnitManager() {
+    return this.unitManager;
   }
   toString(): string {
     return 'Grocy';
-  }
-  async getAllUnits(): Promise<Unit[]> {
-    const options: Options<GetObjectsByEntityData, true> = {
-      path: {
-        entity: 'quantity_units',
-      },
-    };
-    const response = await getObjectsByEntity(options);
-    logger.debug(`Request to Grocy: ${JSON.stringify(options)}`);
-    logger.debug('Response from Grocy:', response);
-    if (!response.data) {
-      throw new Error('No data in response from Grocy');
-    }
-
-    return GrocyUnitsToUnits(response.data as QuantityUnit[]);
-  }
-  async getUnitById(id: string): Promise<Unit | null> {
-    const units = await this.getUnitsByQuery([`id=${id}`]);
-    if (units.length > 1) throw new Error(`Multiple units found with id ${id}, expected one`);
-    return units.shift() || null;
-  }
-  async getUnitByName(name: string): Promise<Unit | null> {
-    const units = await this.getUnitsByQuery([`name=${name}`]);
-    if (units.length > 1) throw new Error(`Multiple units found with name ${name}, expected one`);
-    return units.shift() || null;
-  }
-
-  async focUnit(name: string, pluralName: string): Promise<Unit> {
-    logger.debug(`Fetch or creating unit with name: ${name}`);
-    const unit = await this.getUnitByName(name);
-    if (unit) {
-      return unit;
-    }
-    // If the unit does not exist, create it
-    const newQuantityUnit: QuantityUnit = {
-      name,
-      name_plural: pluralName,
-    };
-    const options: Options<PostObjectsByEntityData, true> = {
-      path: {
-        entity: 'quantity_units',
-      },
-      body: newQuantityUnit,
-    };
-    logger.debug(`Not found, creating unit request: ${JSON.stringify(options)}`);
-    const result = await postObjectsByEntity(options);
-    if (!result.data.created_object_id) throw new Error(`Failed to create unit with name ${name}`);
-    const newUnit = await this.getUnitById(result.data.created_object_id.toString());
-    if (!newUnit) throw new Error(`Failed to create unit with name ${name}`);
-    return newUnit;
-  }
-
-  async updateUnit(unit: Unit): Promise<void> {
-    logger.debug(`Updating unit with name: ${unit.name}`);
-    const grocyUnit = unitToGrocyUnit(unit);
-    if (!grocyUnit.id) throw new Error(`Unit with name ${unit.name} does not have an ID`);
-    const options: Options<PutObjectsByEntityByObjectIdData, true> = {
-      body: grocyUnit,
-      path: {
-        entity: 'quantity_units',
-        objectId: grocyUnit.id,
-      },
-    };
-    await putObjectsByEntityByObjectId(options);
-  }
-
-  private async getUnitsByQuery(query: string[]): Promise<Unit[]> {
-    const options: Options<GetObjectsByEntityData, true> = {
-      path: {
-        entity: 'quantity_units',
-      },
-      query: {
-        'query[]': query,
-      },
-    };
-    const response = await getObjectsByEntity(options);
-    logger.debug(`Request to Grocy: ${JSON.stringify(options)}`);
-    logger.debug('Response from Grocy:', response);
-    if (!response.data) {
-      throw new Error('No data in response from Grocy');
-    }
-
-    return GrocyUnitsToUnits(response.data as QuantityUnit[]);
   }
 }
 
---
+++ src/food-apps/implementations/grocy/grocy-unit-manager.ts
@@ -0,0 +1,102 @@
+import {
+  getObjectsByEntity,
+  GetObjectsByEntityData,
+  Options,
+  postObjectsByEntity,
+  PostObjectsByEntityData,
+  putObjectsByEntityByObjectId,
+  PutObjectsByEntityByObjectIdData,
+  QuantityUnit,
+} from '../../../api-clients/grocy';
+
+import logger from '../../../utils/logger';
+import { UnitManagerBase } from '../../base/food-app';
+import { Unit } from '../../base/food-app-types';
+import { GrocyUnitsToUnits, unitToGrocyUnit } from './grocy-utils';
+
+export class GrocyUnitManager implements UnitManagerBase {
+  async getAllUnits(): Promise<Unit[]> {
+    const options: Options<GetObjectsByEntityData, true> = {
+      path: {
+        entity: 'quantity_units',
+      },
+    };
+    const response = await getObjectsByEntity(options);
+    logger.debug(`Request to Grocy: ${JSON.stringify(options)}`);
+    logger.debug('Response from Grocy:', response);
+    if (!response.data) {
+      throw new Error('No data in response from Grocy');
+    }
+
+    return GrocyUnitsToUnits(response.data as QuantityUnit[]);
+  }
+  async getUnitById(id: string): Promise<Unit | null> {
+    if (Number.isNaN(Number(id))) throw new Error(`Invalid ID: ${id}`);
+    const units = await this.getUnitsByQuery([`id=${id}`]);
+    if (units.length > 1) throw new Error(`Multiple units found with id ${id}, expected one`);
+    return units.shift() || null;
+  }
+  async getUnitByName(name: string): Promise<Unit | null> {
+    const units = await this.getUnitsByQuery([`name=${name}`]);
+    if (units.length > 1) throw new Error(`Multiple units found with name ${name}, expected one`);
+    return units.shift() || null;
+  }
+
+  async focUnit(name: string, pluralName: string): Promise<Unit> {
+    logger.debug(`Fetch or creating unit with name: ${name}`);
+    const unit = await this.getUnitByName(name);
+    if (unit) {
+      return unit;
+    }
+    // If the unit does not exist, create it
+    const newQuantityUnit: QuantityUnit = {
+      name,
+      name_plural: pluralName,
+    };
+    const options: Options<PostObjectsByEntityData, true> = {
+      path: {
+        entity: 'quantity_units',
+      },
+      body: newQuantityUnit,
+    };
+    logger.debug(`Not found, creating unit request: ${JSON.stringify(options)}`);
+    const result = await postObjectsByEntity(options);
+    if (!result.data.created_object_id) throw new Error(`Failed to create unit with name ${name}`);
+    const newUnit = await this.getUnitById(result.data.created_object_id.toString());
+    if (!newUnit) throw new Error(`Failed to create unit with name ${name}`);
+    return newUnit;
+  }
+
+  async updateUnit(unit: Unit): Promise<void> {
+    logger.debug(`Updating unit with name: ${unit.name}`);
+    const grocyUnit = unitToGrocyUnit(unit);
+    if (!grocyUnit.id) throw new Error(`Unit with name ${unit.name} does not have an ID`);
+    const options: Options<PutObjectsByEntityByObjectIdData, true> = {
+      body: grocyUnit,
+      path: {
+        entity: 'quantity_units',
+        objectId: grocyUnit.id,
+      },
+    };
+    await putObjectsByEntityByObjectId(options);
+  }
+
+  private async getUnitsByQuery(query: string[]): Promise<Unit[]> {
+    const options: Options<GetObjectsByEntityData, true> = {
+      path: {
+        entity: 'quantity_units',
+      },
+      query: {
+        'query[]': query,
+      },
+    };
+    const response = await getObjectsByEntity(options);
+    logger.debug(`Request to Grocy: ${JSON.stringify(options)}`);
+    logger.debug('Response from Grocy:', response);
+    if (!response.data) {
+      throw new Error('No data in response from Grocy');
+    }
+
+    return GrocyUnitsToUnits(response.data as QuantityUnit[]);
+  }
+}
