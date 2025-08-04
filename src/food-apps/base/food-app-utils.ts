d9974dd91bbdb35958cb6950111c191b3e08e46c
Harm Ellis <harmellis@outlook.com>
2025-08-04 10:55:30 +0000

    Moved all Unit related functions to a seperate abstract class.

--- src/food-apps/base/food-app-utils.ts
+++ src/food-apps/base/food-app-utils.ts
@@ -3,12 +3,14 @@ import { FoodAppBase } from './food-app';
 
 export async function syncUnits(appFrom: FoodAppBase, appTo: FoodAppBase): Promise<void> {
   logger.info(`Syncing units from ${appFrom.toString()} to ${appTo.toString()}`);
-  const units = await appFrom.getAllUnits();
+  const appFromUnitManager = appFrom.getUnitManager();
+  const appToUnitManager = appTo.getUnitManager();
+  const units = await appFromUnitManager.getAllUnits();
   for (const unit of units) {
     logger.debug(`Syncing unit: ${unit.name}`);
-    const focUnit = await appTo.focUnit(unit.name, unit.pluralName || '');
+    const focUnit = await appToUnitManager.focUnit(unit.name, unit.pluralName || '');
     unit.id = focUnit.id; // Ensure the unit has an ID for updating
-    await appTo.updateUnit(unit);
+    await appToUnitManager.updateUnit(unit);
   }
   logger.info(
     `Synced ${units.length} unit(s) from ${appFrom.toString()} to ${appTo.toString()} successfully`,
@@ -16,7 +18,8 @@ export async function syncUnits(appFrom: FoodAppBase, appTo: FoodAppBase): Promi
 }
 
 export async function formatUnits(app: FoodAppBase): Promise<void> {
-  const units = await app.getAllUnits();
+  const appUnitManager = app.getUnitManager();
+  const units = await appUnitManager.getAllUnits();
   const unitsToFormat = units.filter(
     (unit) =>
       unit?.name.toLocaleLowerCase().localeCompare(unit?.name) ||
@@ -26,7 +29,7 @@ export async function formatUnits(app: FoodAppBase): Promise<void> {
     logger.debug(`Formatting unit: ${unit.name}`);
     unit.name = unit.name.toLocaleLowerCase();
     unit.pluralName = unit.pluralName?.toLocaleLowerCase();
-    await app.updateUnit(unit);
+    await appUnitManager.updateUnit(unit);
   }
   logger.info(`Formatted ${unitsToFormat.length} unit(s) in ${app.toString()}`);
 }
--- src/food-apps/base/food-app.ts
+++ src/food-apps/base/food-app.ts
@@ -2,6 +2,10 @@ import { Unit } from './food-app-types';
 
 export abstract class FoodAppBase {
   abstract toString(): string; // Returns the name of the food app
+  abstract getUnitManager(): UnitManagerBase;
+}
+
+export abstract class UnitManagerBase {
   abstract getAllUnits(): Promise<Unit[]>;
   abstract getUnitById(id: string): Promise<Unit | null>;
   abstract getUnitByName(name: string): Promise<Unit | null>;
